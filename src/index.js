d99476841cf25ff2e9c55ac168e7c604e7a07946
leestana01 <leestana01@naver.com>
2024-05-27 18:38:27 +0900

    feat : 전역 세팅 초기화

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { Reset } from 'styled-reset';
+import Globalfonts from './Globalfonts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <>
+    <Reset/>
+    <App />
+    <Globalfonts/>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
