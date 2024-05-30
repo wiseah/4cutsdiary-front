99050abc144cb489bad672d50cf17931bd0c16e7
wiseah <hyunah2765@gmail.com>
2024-05-30 15:01:35 +0900

    feat: 기능소개 페이지 구현 및 푸터 수정

--- src/styles/Mobile.js
+++ src/styles/Mobile.js
@@ -6,42 +6,36 @@ import { BiCopyright } from "react-icons/bi";
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
 
-const Container=styled.div`
-    display:flex;
-    justify-content:center;
-    align-items:center;
-
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     overflow: auto;
 `;
 
-const Content=styled.div`
-    width:100%;
-    max-width:390px;
-    min-height:100vh;
-
-    /* overflow:auto; */
-    box-shadow:0px 0px 32px #0000002f;
+const Content = styled.div`
+    width: 100%;
+    max-width: 390px;
+    min-height: 100vh;
+    box-shadow: 0px 0px 32px #0000002f;
     background-color: #ffffff;
-
-    /* padding-bottom: 152px; //footer높이만큼 패딩 줘야함. 그래야 내용 다보임. (근데 160으로 하니까 흰부분 생겨서 152px로 고정함.) */
 `;
 
-// 01.home Header
-const Header=styled.div`
-    display:flex;
-    align-items:center;
-    justify-content:space-between;
-
-    height:40px;
-
-    padding:16px 20px;
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 40px;
+    padding: 16px 20px;
 `;
+
 const TitleText = styled.div`
     display: flex;
     align-items: center;
     padding-left: 14px;
     cursor: pointer;
 `;
+
 const Title = styled.div`
     color: #F48B9F;
     font-weight: 700;
@@ -61,9 +55,8 @@ const TitleButtons = styled.div`
     font-weight: 400;
     font-size: 14px;
     font-family: Inter;
-    /* width: 166px;
-    height: 18px; */
 `;
+
 const IntroButton = styled.button`
     background: none;
     border: none;
@@ -73,6 +66,7 @@ const IntroButton = styled.button`
     font-family: Inter;
     cursor: pointer;
 `;
+
 const SuportImageListButton = styled.button`
     background: none;
     border: none;
@@ -83,61 +77,65 @@ const SuportImageListButton = styled.button`
 `;
 
 const Footer = styled.div`
-    /* position: fixed; */
-    /* max-width: 390px;
-    min-width: 360px; */
     width: 100%;
     height: 160px;
-    /* bottom: 0; */
-    /* left: 50%; */
-    
     padding-top: 30px;
     gap: 16px;
-    /* transform: translateX(-50%); */
     background-color: #4D3338;
-
     font-weight: 400;
     font-size: 18px;
     box-shadow: 0px -15px 25px 0px rgba(0, 0, 0, 0.04);
 `;
+
 const ServiceName = styled.div`
-color: #ffffff;
-font-size: 40px;
-font-weight: 500;
-/* margin-top: 16px; */
-text-align: center;
+    color: #ffffff;
+    font-size: 40px;
+    font-weight: 700;
+    text-align: center;
 `;
+
 const Member = styled.div`
-display:flex;
-align-items:center;
-justify-content:space-around;
-margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 30px;
 `;
+
 const Leader = styled.div`
-color: #ffffff;
-font-size: 12px;
-font-weight: 500;
+    color: #ffffff;
+    font-size: 12px;
+    font-weight: 500;
+
+    div {
+        margin-top: 4px; // 간격 조정을 위해 추가
+    }
 `;
+
 const Follower = styled.div`
-color: #ffffff;
-font-size: 12px;
-font-weight: 500;
+    color: #ffffff;
+    font-size: 12px;
+    font-weight: 500;
+
+    div {
+        margin-top: 4px; // 간격 조정을 위해 추가
+    }
 `;
 
 const Team = styled.div`
-color: #ffffff;
-font-size: 12px;
-font-weight: 500;
-margin-top: 14px;
-text-align: center;
+    color: #ffffff;
+    font-size: 12px;
+    font-weight: 500;
+    margin-top: 10px;
+    text-align: center;
 `;
+
 const TeamText = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
 `;
 
-const Mobile=()=>{
+const Mobile = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
@@ -154,7 +152,7 @@ const Mobile=()=>{
         navigate("/");
     }
 
-    return(
+    return (
         <>
             <Container>
                 <Content>
@@ -164,7 +162,7 @@ const Mobile=()=>{
                             <Title>네컷일기</Title>
                         </TitleText>
                         <TitleButtons>
-                            <IntroButton>
+                            <IntroButton onClick={() => navigate('/feature')}>
                                 기능소개
                             </IntroButton>
                             {
@@ -174,9 +172,6 @@ const Mobile=()=>{
                                     </IntroButton>
                                 )
                             }
-                            {/* <SuportImageListButton>
-                                지원 이미지 목록
-                            </SuportImageListButton> */}
                         </TitleButtons>
                     </Header>
                     <Outlet />
@@ -186,28 +181,25 @@ const Mobile=()=>{
                         </ServiceName>
                         <Member>
                             <Leader>
-                                총괄/백엔드 팀장: 이수혁
-                                <br />
-                                프론트엔드 팀장: 김현아
+                                <div>총괄/백엔드 팀장: 이수혁</div>
+                                <div>프론트엔드 팀장: 김현아</div>
                             </Leader>
                             <Follower>
-                                백엔드: 고선민, 류동현
-                                <br />
-                                프론트: 김규린, 조윤해
+                                <div>백엔드: 고선민, 류동현</div>
+                                <div>프론트: 김규린, 조윤해</div>
                             </Follower>
                         </Member>
                         <Team>
                             <TeamText>
-                            <BiCopyright size="14px" style={{ marginRight: '2px', color: '#FFFFFF', marginBottom: '1px'}}/>
+                                <BiCopyright size="14px" style={{ marginRight: '2px', color: '#FFFFFF', marginBottom: '1px'}}/>
                                 한국외국어대학교 멋쟁이사자처럼 미니프로젝트 2팀 
                             </TeamText>
-                            
                         </Team>
                     </Footer> 
                 </Content>
             </Container>
         </>
-    )
+    );
 };
 
 export default Mobile;
